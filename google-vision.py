ad8a2e45ab86895c51be47de85e63b0ab5628d6e
justin-segler <justinsegler@Justins-MacBook-Pro.local>
2017-12-18 13:59:55 -0600

    connected to google apis and can send an image and retrieve a list of features of the image. requires some config... must connect to an active google cloud account and must have environment variable called VISION_API that is a proper API key connected to an account

--- google-vision.py
+++ google-vision.py
@@ -1,1 +1,42 @@
-# Test
+import argparse
+import os
+
+from utils import Service, encode_image
+
+def main(photo_file):
+    """Run a face detection request on a single image"""
+
+    # Stored API key as environment variable called VISION_API
+    #
+    # Generate API key from Google Cloud Dashboard -> APIs and Services ->
+    # Credentials -> Create Credentials -> API Key
+
+    access_token = os.environ.get('VISION_API')
+    service = Service('vision', 'v1', access_token=access_token)
+
+    with open(photo_file, 'rb') as image:
+        base64_image = encode_image(image)
+        body = {
+            'requests': [{
+                'image': {
+                    'content': base64_image,
+                },
+                'features': [
+                    {
+                        'type': 'LABEL_DETECTION',
+                        'maxResults': 1000,
+                    }
+                ]
+
+            }]
+        }
+        response = service.execute(body=body)
+        labels = response['responses'][0]['labelAnnotations']
+        for label in labels:
+            print(label['description'] + ": " + str(label['score']))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image_file', help='The image you\'d like to detect text.')
+    args = parser.parse_args()
+    main(args.image_file)
