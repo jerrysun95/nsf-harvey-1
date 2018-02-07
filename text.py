c6d9a38bb536bfa2bdf633a585cc115e84ec9b4a
justin-segler <justinsegler@wireless-10-145-123-61.public.utexas.edu.ten.thomsonreuters.com>
2018-02-07 12:08:55 -0600

    small tweaks

--- text.py
+++ text.py
@@ -13,10 +13,16 @@ en_stop = get_stop_words('en')
 p_stemmer = PorterStemmer()
 
 # create sample documents
-doc_a = "Thanks to a hands-on guide introducing programming fundamentals alongside topics in computational linguistics, plus comprehensive API documentation, NLTK is suitable for linguists, engineers, students, educators, researchers, and industry users alike. NLTK is available for Windows, Mac OS X, and Linux. Best of all, NLTK is a free, open source, community-driven project"
+doc_a = 'The Super Bowl is the annual championship game of the National Football League (NFL). The game is the culmination of a regular season that begins in the late summer of the previous calendar year. Normally, Roman numerals are used to identify each game, rather than the year in which it is held. For example, Super Bowl I was played on January 15, 1967, following the 1966 regular season. The sole exception to this naming convention tradition occurred with Super Bowl 50, which was played on February 7, 2016, following the 2015 regular season, and the following year, the nomenclature returned to Roman numerals for Super Bowl LI, following the 2016 regular season. The most recent Super Bowl was Super Bowl LII, on February 4, 2018, following the 2017 regular season. The game was created as part of a merger agreement between the NFL and its then-rival league, the American Football League (AFL). It was agreed that the two leagues champion teams would play in the AFLNFL'
+
+doc_b = 'World Championship Game until the merger was to officially begin in 1970. After the merger, each league was redesignated as a conference, and the game has since been played between the conference champions to determine the NFLs league champion. Currently, the National Football Conference (NFC) leads the league with 27 wins to 25 wins for the American Football Conference (AFC). The Pittsburgh Steelers have the most Super Bowl championship titles, with six. The New England Patriots have the most Super Bowl appearances, with ten. Charles Haley and Tom Brady both have five Super Bowl rings, which is the record for the most rings won by a single player. The day on which the Super Bowl is played, now considered by some as an unofficial American national holiday,[1][2] is called Super Bowl Sunday. It is the second-largest day for U.S. food consumption, after Thanksgiving Day.[3] In addition, the Super Bowl has frequently been the most-watched American television broadcast of the year'
+
+doc_c = 'the seven most-watched broadcasts in U.S. television history are Super Bowls.[4] In 2015, Super Bowl XLIX became the most-watched American television program in history with an average audience of 114.4 million viewers, the fifth time in six years the game had set a record, starting with the 2010 Super Bowl, which itself had taken over the number-one spot held for 27 years by the final episode of M*A*S*H.[5][6][7] The Super Bowl is also among the most-watched sporting events in the world, almost all audiences being North American, and is second to soccers UEFA Champions League final as the most watched annual sporting event worldwide.[8]'
+
+doc_d = 'The NFL restricts the use of its Super Bowl trademark; it is frequently called the Big Game or other generic terms by non-sponsoring corporations. Because of the high viewership, commercial airtime during the Super Bowl broadcast is the most expensive of the year, leading to companies regularly developing their most expensive advertisements for this broadcast. As a result, watching and discussing the broadcasts commercials has become a significant aspect of the event.[9] In addition, popular singers and musicians including Mariah Carey, Michael Jackson, Madonna, Prince, Beyonc, Paul McCartney, The Rolling Stones, The Who, Whitney Houston, and Lady Gaga have performed during the events pre-game and halftime ceremonies.'
 
 # compile sample documents into a list
-doc_set = [doc_a]
+doc_set = [doc_a, doc_b, doc_c, doc_d]
 
 # list for tokenized documents in loop
 texts = []
@@ -46,4 +52,4 @@ corpus = [dictionary.doc2bow(text) for text in texts]
 # generate LDA model
 ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=2, id2word = dictionary, passes=20)
 
-print(ldamodel.print_topics(num_topics=3, num_words=8))
+print(ldamodel.print_topics(num_topics=2, num_words=15))
