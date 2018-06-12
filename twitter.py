2bb7202d57e93f04d4bc07f76a9c1e0fb9ca7726
brodgers <rodgers.benjamin@utexas.edu>
2018-06-12 00:34:41 -0500

    added twitter samples

--- twitter.py
+++ twitter.py
@@ -1,5 +1,6 @@
-import json, os, ast, gzip, keyring, requests
+import json, os, ast, gzip, keyring, requests, random
 import google_vision as gv
+import bz2
 
 # Grabs relevant information from a tweet and returns result
 # If the tweet does not contain an image, returns None
@@ -24,28 +25,70 @@ def parse_tweets(path):
 	for filename in os.listdir(path):
 		if '.gz' in filename:
 			file = gzip.open(path + '/' + filename.strip(' \t\n\r'), 'rb')
-			for line in file:
-				tweet = ast.literal_eval(line)
-				d = parse_tweet(tweet)
-				if d != None:
-					tweets_with_links.append(d)
-	with open('output/tweets_with_links.json', 'w') as f:
+			# for line in file:
+			# 	tweet = ast.literal_eval(line)
+			# 	d = parse_tweet(tweet)
+			# 	if d != None:
+			# 		tweets_with_links.append(d)
+		elif '.bz2' in filename:
+			with bz2.BZ2File(path + '/' + filename) as file:
+				# for line in file.readlines():
+				line = file.readline()
+				while line:
+					print('hello')
+					tweet = ast.literal_eval(line)
+					d = parse_tweet(tweet)
+					if d != None:
+						tweets_with_links.append(d)
+
+					line = file.readline()
+	with open('output/tweets_with_links_s3.json', 'w') as f:
 		f.write(json.dumps(tweets_with_links, indent=4))
 
 # Retrieves image data from an image url
-def get_image_from_url():
+def get_image_from_url(path, out):
 	labels = []
-	with open('output/tweets_with_links.json') as f:
-		data = json.load(f.read())
+	with open(path, 'r') as f:
+		data = json.loads(f.read())
 		for obj in data:
 			print(obj)
 			imageLabels = {}
 			image = requests.get(obj['media_url']).content
-			imageLabels = gv.vision_from_data_image(str(obj['id']), image)
-			labels.append(imageLabels)
-	with open('output/tweets_gv.json', 'w') as g:
+			try:
+				imageLabels = gv.vision_from_data_image(str(obj['id']), image)
+				labels.append(imageLabels)
+			except:
+				pass
+	with open(out, 'w') as g:
 		g.write(json.dumps(labels, indent=4))
 
-get_image_from_url()
+def get_random_sample(path, num_tweets, out):
+	print('Getting random sample')
+	tweets = []
+	files = [x for x in os.listdir(path) if '.bz2' in x]
+	num_samples = 10
+	samples = random.sample(range(len(files)), num_samples)
+
+	for s in samples:
+		print('Sampling zip ' + str(s))
+		file = files[s]
+		count = 0
+		with bz2.BZ2File(path + '/' + file) as f:
+			line = f.readline()
+			while line and count < num_tweets:
+				tweet = ast.literal_eval(line)
+				d = parse_tweet(tweet)
+				if d != None:
+					tweets.append(d)
+					count += 1
+
+				line = f.readline()
+
+	res = random.sample(tweets, num_tweets)
+	with open(out, 'w') as f:
+		f.write(json.dumps(res, indent=4))
+
+# get_image_from_url()
 
 # parse_tweets('tweets')
+# get_random_sample('tweets', 10, 'tweets/random_sample_test.json')
